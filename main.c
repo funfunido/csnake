33df07fff302a161a503988f6be9246997210c81
Fun <161741261+funfunido@users.noreply.github.com>
2025-11-06 17:08:23 +0000

    updating stuff and adding more keys lol

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main() {
-	printf("hello cyberpatriots\n");
+	printf("hello cs class\n");
 
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - input keys");
+    InitWindow(screenWidth, screenHeight, "cSnake");
 
     Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
 
@@ -17,14 +17,13 @@ int main() {
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    while (!WindowShouldClose()) {   // Detect window close button or ESC key
         // Update
         //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        if (IsKeyDown(KEY_RIGHT || KEY_D)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT || KEY_A)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP || KEY_W)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN || KEY_S)) ballPosition.y += 2.0f;
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -33,7 +32,7 @@ int main() {
 
             ClearBackground(RAYWHITE);
 
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+            DrawText("Move snake with arrow keys", 10, 10, 20, DARKGRAY);
 
             DrawCircleV(ballPosition, 50, MAROON);
 
