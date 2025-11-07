cdeebabbbab8df5356c1024b09c1612be932fcb3
Ellie <ellieputnam5@gmail.com>
2025-11-07 10:04:42 -0700

    codealicious definition makes terezi loco

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include "include/raylib.h"
 #include <stdio.h>
 
+typedef enum direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+ } dir_t;
 
 int main() {
     const int screenWidth = 640;
     const int screenHeight = 640;
+
+    dir_t snakeMove;
 
     InitWindow(screenWidth, screenHeight, "cSnake");
 
@@ -15,10 +23,25 @@ int main() {
 
     while (!WindowShouldClose()) {
 
-        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) recX += 2;
-        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) recX -= 2;
-        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) recY -= 2;
-        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) recY += 2;
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) snakeMove = RIGHT;
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) snakeMove = LEFT;
+        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) snakeMove = UP;
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) snakeMove = DOWN;
+
+        switch(snakeMove) {
+            case RIGHT:
+                recX += 10;
+                break;
+            case LEFT:
+                recX -= 10;
+                break;
+            case UP:
+                recY -= 10;
+                break;
+            case DOWN:
+                recY += 10;
+                break;
+        }
 
         BeginDrawing();
 
